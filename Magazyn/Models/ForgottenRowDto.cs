7795d1aca439e05daaa1f6766d3c40e0956c04ff
Mateusz <75093547+MeG123321@users.noreply.github.com>
2026-04-18 17:08:45 +0200

    Delete Magazyn directory

--- Magazyn/Models/ForgottenRowDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Magazyn.Models.Dtos;
-
-public class ForgottenRowDto
-{
-    public long Id { get; set; }
-    public string? FullNameAfterForget { get; set; }
-    public string? DataZapomnienia { get; set; }
-    public string? ZapomnialUserId { get; set; }
-}
