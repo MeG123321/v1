1abbb039a19ec1749826fcadd8c0369ec6f207b5
Mateusz <75093547+MeG123321@users.noreply.github.com>
2026-06-01 13:53:54 +0200

    jhui

---
+++ Magazyn.Tests/Magazyn.Tests/Testydodawanie.cs
@@ -0,0 +1,338 @@
+﻿using Magazyn.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Xunit;
+
+namespace Magazyn.Tests.Scenariusze
+{
+    public class DodawanieUzytkownikaTests
+    {
+        private UserRegistrationDto UtworzPoprawnegoUzytkownika()
+        {
+            return new UserRegistrationDto
+            {
+                Username = "testprac01",
+                Password = "Test123!",
+                FirstName = "Marek",
+                LastName = "Nowak",
+                Email = "marek.nowak01@example.com",
+                Pesel = "85021412345",
+                NrTelefonu = "600700800",
+                Plec = "Mężczyzna",
+                Status = "Aktywny",
+
+                Rola = "Użytkownik",
+                DataUrodzenia = new DateOnly(1985, 2, 14),
+                Miejscowosc = "Poznań",
+                KodPocztowy = "60-101",
+                Ulica = "Długa",
+                NrPosesji = "12A",
+                NrLokalu = "5"
+            };
+        }
+
+        private (bool wynikWalidacji, List<ValidationResult> listaBledowWalidacji) WalidujModel(object model)
+        {
+            var listaBledowWalidacji = new List<ValidationResult>();
+            var kontekstWalidacji = new ValidationContext(model);
+            bool wynikWalidacji = Validator.TryValidateObject(model, kontekstWalidacji, listaBledowWalidacji, validateAllProperties: true);
+            return (wynikWalidacji, listaBledowWalidacji);
+        }
+
+        private static void SprawdzCzyZawieraBladDlaPola(List<ValidationResult> listaBledowWalidacji, string nazwaPola)
+        {
+            Assert.Contains(listaBledowWalidacji, blad => blad.MemberNames.Contains(nazwaPola));
+        }
+
+      
+        [Fact]
+        public void TC_1_DodanieUzytkownika_Sukces()
+        {
+            UserRegistrationDto poprawnyUzytkownik = UtworzPoprawnegoUzytkownika();
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(poprawnyUzytkownik);
+
+            Assert.True(wynikWalidacji);
+            Assert.Empty(listaBledowWalidacji);
+        }
+
+      
+        [Fact]
+        public void TC_2_DodanieUzytkownika_BezPolOpcjonalnych()
+        {
+            UserRegistrationDto poprawnyUzytkownik = UtworzPoprawnegoUzytkownika();
+            poprawnyUzytkownik.Ulica = "";
+            poprawnyUzytkownik.NrLokalu = "";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(poprawnyUzytkownik);
+
+            Assert.True(wynikWalidacji);
+            Assert.Empty(listaBledowWalidacji);
+        }
+
+      
+        [Fact]
+        public void TC_3_AnulowanieOperacji_WalidacjaPoprawna()
+        {
+            UserRegistrationDto poprawnyUzytkownik = UtworzPoprawnegoUzytkownika();
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(poprawnyUzytkownik);
+
+            Assert.True(wynikWalidacji);
+            Assert.Empty(listaBledowWalidacji);
+        }
+
+      
+        [Fact]
+        public void TC_4_WszystkiePolaWymaganePuste()
+        {
+            UserRegistrationDto pustyUzytkownik = new UserRegistrationDto();
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(pustyUzytkownik);
+
+            Assert.False(wynikWalidacji);
+            Assert.NotEmpty(listaBledowWalidacji);
+        }
+
+        
+        [Fact]
+        public void TC_5_PustyLogin()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Username = "";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.Username));
+        }
+
+        
+        [Fact]
+        public void TC_6_PustyEmail()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Email = "";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.Email));
+        }
+
+        
+        [Fact]
+        public void TC_7_PustyPesel()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Pesel = "";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.Pesel));
+        }
+
+       
+        [Fact]
+        public void TC_8_PustaDataUrodzenia()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.DataUrodzenia = null;
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.DataUrodzenia));
+        }
+
+        
+        [Fact]
+        public void TC_9_PustyTelefon()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.NrTelefonu = "";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.NrTelefonu));
+        }
+
+        
+        [Fact]
+        public void TC_10_LoginZaKrotki()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Username = "abc";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.Username));
+        }
+
+      
+        [Fact]
+        public void TC_11_HasloZaKrotkie()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Password = "Ab1!";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.Password));
+        }
+
+        
+        [Fact]
+        public void TC_12_HasloBezZnakuSpecjalnego()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Password = "Test1234";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.Password));
+        }
+
+        
+        [Fact]
+        public void TC_13_EmailBezMalpy()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Email = "marek.nowak01example.com";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.Email));
+        }
+
+       
+        [Fact]
+        public void TC_14_PeselZLitera()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Pesel = "85021412A4";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.Pesel));
+        }
+
+        
+        [Fact]
+        public void TC_15_TelefonZaKrotki()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.NrTelefonu = "60070080";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.NrTelefonu));
+        }
+
+      
+        [Fact]
+        public void TC_16_KodPocztowyZlyFormat()
+        {
+            UserRegistrationDto uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.KodPocztowy = "60101";
+
+            var (wynikWalidacji, listaBledowWalidacji) = WalidujModel(uzytkownik);
+
+            Assert.False(wynikWalidacji);
+            SprawdzCzyZawieraBladDlaPola(listaBledowWalidacji, nameof(UserRegistrationDto.KodPocztowy));
+        }
+        [Fact]
+        public void TC_17_Login_ZawieraSpacje_Blad()
+        {
+            var uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Username = "test user";
+
+            var (wynik, bledy) = WalidujModel(uzytkownik);
+
+            Assert.False(wynik);
+            SprawdzCzyZawieraBladDlaPola(bledy, nameof(UserRegistrationDto.Username));
+        }
+
+        [Fact]
+        public void TC_18_Pesel_ZaDlugi_Blad()
+        {
+            var uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Pesel = "850101123456";
+
+            var (wynik, bledy) = WalidujModel(uzytkownik);
+
+            Assert.False(wynik);
+            SprawdzCzyZawieraBladDlaPola(bledy, nameof(UserRegistrationDto.Pesel));
+        }
+
+        [Fact]
+        public void TC_19_Telefon_ZaDlugi_Blad()
+        {
+            var uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.NrTelefonu = "123456789123";
+
+            var (wynik, bledy) = WalidujModel(uzytkownik);
+
+            Assert.False(wynik);
+            SprawdzCzyZawieraBladDlaPola(bledy, nameof(UserRegistrationDto.NrTelefonu));
+        }
+
+        [Fact]
+        public void TC_20_KodPocztowy_Litery_Blad()
+        {
+            var uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.KodPocztowy = "AA-AAA";
+
+            var (wynik, bledy) = WalidujModel(uzytkownik);
+
+            Assert.False(wynik);
+            SprawdzCzyZawieraBladDlaPola(bledy, nameof(UserRegistrationDto.KodPocztowy));
+        }
+
+       
+
+        [Fact]
+        public void TC_21_Haslo_BrakMalejLitery_Blad()
+        {
+            var uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Password = "TEST123!";
+
+            var (wynik, bledy) = WalidujModel(uzytkownik);
+
+            Assert.False(wynik);
+            SprawdzCzyZawieraBladDlaPola(bledy, nameof(UserRegistrationDto.Password));
+        }
+
+        [Fact]
+        public void TC_22_Haslo_BrakWielkiejLitery_Blad()
+        {
+            var uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Password = "test123!";
+
+            var (wynik, bledy) = WalidujModel(uzytkownik);
+
+            Assert.False(wynik);
+            SprawdzCzyZawieraBladDlaPola(bledy, nameof(UserRegistrationDto.Password));
+        }
+
+        [Fact]
+        public void TC_23_Haslo_BrakZnakuSpecjalnego_Blad()
+        {
+            var uzytkownik = UtworzPoprawnegoUzytkownika();
+            uzytkownik.Password = "Test1234";
+
+            var (wynik, bledy) = WalidujModel(uzytkownik);
+
+            Assert.False(wynik);
+            SprawdzCzyZawieraBladDlaPola(bledy, nameof(UserRegistrationDto.Password));
+        }
+    }
+}
---
+++ Magazyn.Tests/Magazyn.Tests/Testyedycja.cs
@@ -0,0 +1,271 @@
+﻿using Magazyn.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Xunit;
+
+namespace Magazyn.Tests.Scenariusze
+{
+    public class EdycjaUzytkownika_WalidacjaTests
+    {
+        private UserVm Poprawny()
+        {
+            return new UserVm
+            {
+                Id = 3,
+                Username = "user03",
+                Password = "Test123!", 
+                FirstName = "Piotr",
+                LastName = "Wiśniewski",
+                Email = "piotr.wisniewski@example.com",
+                Pesel = "92010978917",
+                DataUrodzenia = new DateOnly(1992, 1, 9),
+                NrTelefonu = "700800900",
+                Plec = "Mężczyzna",
+                Status = "Aktywny",
+                Miejscowosc = "Poznań",
+                KodPocztowy = "60-101",
+                NrPosesji = "12A",
+                Ulica = "Długa",
+                NrLokalu = "5"
+            };
+        }
+
+        private (bool ok, List<ValidationResult> wyniki) Waliduj(object model)
+        {
+            var wyniki = new List<ValidationResult>();
+            var ctx = new ValidationContext(model);
+            var ok = Validator.TryValidateObject(model, ctx, wyniki, validateAllProperties: true);
+            return (ok, wyniki);
+        }
+
+        private static void AssertHasErrorFor(List<ValidationResult> wyniki, string memberName)
+        {
+            Assert.Contains(wyniki, w => w.MemberNames.Contains(memberName));
+        }
+
+     
+        [Fact]
+        public void TC_24_Edycja_PustePoleImie()
+        {
+            var vm = Poprawny();
+            vm.FirstName = "";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.FirstName));
+        }
+
+        
+        [Fact]
+        public void TC_25_Edycja_BlednyEmail()
+        {
+            var vm = Poprawny();
+            vm.Email = "piotr.wisniewski.at.example.com";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.Email));
+        }
+
+      
+        [Fact]
+        public void TC_26_Edycja_BlednyTelefon()
+        {
+            var vm = Poprawny();
+            vm.NrTelefonu = "12345678";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.NrTelefonu));
+        }
+
+        
+        [Fact]
+        public void TC_27_Edycja_BlednyKodPocztowy()
+        {
+            var vm = Poprawny();
+            vm.KodPocztowy = "12345";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.KodPocztowy));
+        }
+
+      
+        [Fact]
+        public void TC_28_Edycja_BlednyPesel()
+        {
+            var vm = Poprawny();
+            vm.Pesel = "12345ABCDE1";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.Pesel));
+        }
+
+        [Fact]
+        public void TC_29_Edycja_Email_BezMalpy_Blad()
+        {
+            var vm = Poprawny();
+            vm.Email = "test.pl";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.Email));
+        }
+
+        [Fact]
+        public void TC_30_Edycja_Login_ZaKrotki_Blad()
+        {
+            var vm = Poprawny();
+            vm.Username = "abc";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.Username));
+        }
+
+        [Fact]
+        public void TC_31_Edycja_Telefon_Litery_Blad()
+        {
+            var vm = Poprawny();
+            vm.NrTelefonu = "123abc789";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.NrTelefonu));
+        }
+
+        [Fact]
+        public void TC_32_Edycja_Pesel_Litery_Blad()
+        {
+            var vm = Poprawny();
+            vm.Pesel = "85010A12345";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.Pesel));
+        }
+
+        [Fact]
+        public void TC_33_Edycja_KodPocztowy_BrakMyslnika_Blad()
+        {
+            var vm = Poprawny();
+            vm.KodPocztowy = "60101";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.KodPocztowy));
+        }
+
+        [Fact]
+        public void TC_34_Edycja_Imie_Puste_Blad()
+        {
+            var vm = Poprawny();
+            vm.FirstName = "";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.FirstName));
+        }
+
+        [Fact]
+        public void TC_35_Edycja_Nazwisko_Puste_Blad()
+        {
+            var vm = Poprawny();
+            vm.LastName = "";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.LastName));
+        }
+
+        [Fact]
+        public void TC_36_Edycja_Haslo_BezCyfry_Blad()
+        {
+            var vm = Poprawny();
+            vm.Password = "TestHaslo!";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.Password));
+        }
+
+        [Fact]
+        public void TC_37_Edycja_Haslo_BezZnakuSpecjalnego_Blad()
+        {
+            var vm = Poprawny();
+            vm.Password = "Test1234";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.Password));
+        }
+
+        [Fact]
+        public void TC_38_Edycja_Status_Pusty_Blad()
+        {
+            var vm = Poprawny();
+            vm.Status = "";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.Status));
+        }
+
+       
+        [Fact]
+        public void TC_39_Edycja_Miejscowosc_Pusta_Blad()
+        {
+            var vm = Poprawny();
+            vm.Miejscowosc = "";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.Miejscowosc));
+        }
+
+
+        [Fact]
+        public void TC_40_Edycja_NrPosesji_Pusty_Blad()
+        {
+            var vm = Poprawny();
+            vm.NrPosesji = "";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+            AssertHasErrorFor(wyniki, nameof(UserVm.NrPosesji));
+        }
+
+        [Fact]
+        public void TC_41_Edycja_WieleBledowNaraz()
+        {
+            var vm = Poprawny();
+            vm.Email = "zly";
+            vm.Pesel = "123";
+
+            var (ok, wyniki) = Waliduj(vm);
+
+            Assert.False(ok);
+        }
+    }
+}
